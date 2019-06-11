bbeca966e205beb1f1167bb3bfe2709249b7f47a
eriika19 <51388362+eriika19@users.noreply.github.com>
2019-06-11 18:34:56 -0500

    Codigo cifrado y descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,33 @@
 window.cipher = {
-  // ... 
+  encode: (str, n) => {
+    let encodeStr = '';
+    const newStr = str.toUpperCase();
+    for (let i=0; i<newStr.length; i++){
+      if (newStr [i] === ' ' ||
+          newStr [i] === 'Ñ') {
+      encodeStr += newStr[i];
+      } else {
+        let newPosition = ((newStr.charCodeAt(i)-65+n)%26)+65;
+        let newLetter = String.fromCharCode(newPosition);
+        encodeStr += newLetter;
+      }
+    }
+    return encodeStr;
+  },
+
+  decode: (str, n) => {
+    let decodeStr = '';
+    const newStr = str.toUpperCase();
+    for (let i=0; i<newStr.length; i++){
+      if (newStr [i] === ' ' ||
+          newStr [i] === 'Ñ') {
+      decodeStr += newStr[i];
+      } else {
+        let newPosition = newStr.charCodeAt(i)-(n%26);
+        let newLetter = String.fromCharCode(newPosition);
+        decodeStr += newLetter;
+      }
+    }
+    return decodeStr;
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 const home = document.getElementById('logo-home');
 const goHome = () => location.href = home.html;
 home.addEventListener('click',goHome);
+
+
+
+console.log(window.cipher.decode('bcd',1));
